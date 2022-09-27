b4993a54b28de927a44b46d04b24554d296227c9
Ruan Failache <ruanfailache@gmail.com>
2022-09-27 13:33:50 -0300

    feat: add interactive at answer a question

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,60 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Icon> scoreKeeper = const [];
+
+  List<String> questions = const [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int questionNumber = 0;
+
+  answerQuestion(bool answer) {
+    final lastQuestionIndex = questions.length - 1;
+    setState(() {
+      if (questionNumber < lastQuestionIndex) {
+        questionNumber++;
+      }
+
+      if (scoreKeeper.length < questions.length) {
+        scoreKeeper = [
+          ...scoreKeeper,
+          Icon(
+            answer ? Icons.check : Icons.close,
+            color: answer ? Colors.green : Colors.red,
+          ),
+        ];
+      }
+    });
+  }
+
+  buildHeader() {
+    if (scoreKeeper.isEmpty) {
+      return const Text(
+        'Answer a question to appear here the results',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 14,
+          color: Colors.white,
+        ),
+      );
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: scoreKeeper,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,48 +80,63 @@ class HomePage extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Expanded(
+          buildHeader(),
+          Expanded(
             child: Center(
               child: Text(
-                'This is where the question text will go.',
-                style: TextStyle(
+                questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: const TextStyle(
                   fontSize: 24,
                   color: Colors.white,
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
           ),
-          TextButton(
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              backgroundColor: Colors.green,
-            ),
-            onPressed: () {},
-            child: const Text(
-              'True',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
+          CustomButton(
+            text: 'True',
+            onPressed: () => answerQuestion(true),
+            backgroundColor: Colors.green,
           ),
           const SizedBox(height: 10),
-          TextButton(
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              backgroundColor: Colors.red,
-            ),
-            onPressed: () {},
-            child: const Text(
-              'False',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
+          CustomButton(
+            text: 'False',
+            onPressed: () => answerQuestion(false),
+            backgroundColor: Colors.red,
           ),
+          const SizedBox(height: 10),
         ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    required this.backgroundColor,
+  }) : super(key: key);
+
+  final String text;
+  final void Function() onPressed;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        backgroundColor: backgroundColor,
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+        ),
       ),
     );
   }
